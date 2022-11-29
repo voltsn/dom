0b3c5978a15357ad98a792ec33fdfec6ee25d35c
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2022-11-29 16:59:24 +0100

    Minor improvements

--- 1.3.Create/script.js
+++ 1.3.Create/script.js
@@ -1,17 +1,3 @@
-/*
- * Instructions
- * ----------------------------------------------------------
- *
- * Create a section element for each learner with 
- * a random background color. The sections should 
- * contain a paragraph with the name of the learner
- * and appended in the article tag.
- *
- * If the background is dark the text should be white,
- * if the background is light the text should be black.
- *
- * Change the order of the elements on every page load.
- */
 const learners = [
   "Arno Volts",
   "Aurélien Mariaule",
@@ -39,18 +25,23 @@ const learners = [
   "Virginie Dourson",
 ];
 
-// Shuffles the contents an array using the Fisher-Yates
+// Generates a random number between min and max
+// inclusive.
+const randNumber = (max, min) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Shuffles the contents of an array using the Fisher-Yates
 // shuffle algorithm.
 const shuffle = (arr) => {
-  let currentIndex = arr.length - 1;
-  for (; currentIndex >= 0; currentIndex--) {
-    let randomIndex = Math.floor(Math.random() * (currentIndex + 1));
+  let cursor = arr.length - 1;
+  for (; cursor >= 0; cursor--) {
+    let randomIndex = randNumber(cursor, 0);
 
     // Swap values.
     let tmp = arr[randomIndex];
-    arr[randomIndex] = arr[currentIndex];
-    arr[currentIndex] = tmp;
-    console.log(currentIndex);
+    arr[randomIndex] = arr[cursor];
+    arr[cursor] = tmp;
   }
 }
 
@@ -59,41 +50,45 @@ const shuffle = (arr) => {
 // red, green, and blue. Corresponding to the rgb color channels.
 const isLight = (color) => {
   let brightness = (0.2126 * color.red) + (0.72152 * color.green) + (0.0722 * color.blue);
-  return brightness > 127.5 ? true : false;
+  return brightness > 155;
 }
 
 // Returns an object with the three channels 
 // of an rgb color each of which has a random
 // value.
 const randomRgbColor = () => {
-  let red = Math.floor(Math.random() * (255 + 1));
-  let green = Math.floor(Math.random() * (255 + 1));
-  let blue = Math.floor(Math.random() * (255 + 1));
+  let red = randNumber(255,0);
+  let green = randNumber(255, 0);
+  let blue = randNumber(255, 0);
 
   return {red, green, blue};
 }
 
-// Shuffles elements of the array.
-shuffle(learners);
+const main = () => {
+  // Shuffles elements of the array.
+  shuffle(learners);
+  
+  // Populate the article element with the contents
+  // of the array.
+  let article = document.querySelector("article");
+  for (learner of learners) {
+    let section = document.createElement("section");
+    let paragraph = document.createElement("p");
+    let learnersName = document.createTextNode(learner);
+    let color = randomRgbColor(); 
 
-// Populate the article element with the contents
-// of the array.
-let article = document.querySelector("article");
-for (learner of learners) {
-  let section = document.createElement("section");
-  let paragraph = document.createElement("p");
-  let text = document.createTextNode(learner);
+    section.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
 
-  let color = randomRgbColor(); 
-  section.style.backgroundColor = `rgb(${color.red}, ${color.green}, ${color.blue})`;
+    // Color the text white if it is in a dark
+    // background.
+    if (!isLight(color)) {
+      section.style.color = "rgb(255, 255, 255)";
+    }
 
-  // Color the text white if it is in a dark
-  // background.
-  if (!isLight(color)) {
-    section.style.color = "rgb(255, 255, 255)";
+    paragraph.appendChild(learnersName);
+    section.appendChild(paragraph);
+    article.appendChild(section);
   }
-
-  paragraph.appendChild(text);
-  section.appendChild(paragraph);
-  article.appendChild(section);
 }
+
+main();
